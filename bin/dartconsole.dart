cfa949b15680d9f6b57b05796f9b5a01dc34e4e6
adeelzahid1 <developer.adeelzahid@gmail.com>
2022-04-01 23:54:34 +0500

    data added for dart linq

--- bin/dartconsole.dart
+++ bin/dartconsole.dart
@@ -1,14 +1,30 @@
-void main() {
-  Stream<int> stream = boatStream(10);
-  stream.listen((data) =>{
-    print('Received Boat No $data'),
-  });
+import '../bin/model/Customer.dart';
+
+void main(){
+  List<Customer> _modell = customers;
+  var _model;
+
+  _model = _modell.where((element) => element.age! < 20).toList();
+  print(_model);
+
 }
 
-Stream<int> boatStream (int val) async*{
-  for(int i=1; i<=val; i++){
-    print("sent boat number $i");
-    await Future.delayed(Duration(seconds: 2),);
-    yield i;
-  }
-}
+
+
+
+
+
+// void main() {
+//   Stream<int> stream = boatStream(10);
+//   stream.listen((data) =>{
+//     print('Received Boat No $data'),
+//   });
+// }
+
+// Stream<int> boatStream (int val) async*{
+//   for(int i=1; i<=val; i++){
+//     print("sent boat number $i");
+//     await Future.delayed(Duration(seconds: 1),);
+//     yield i;
+//   }
+// }
