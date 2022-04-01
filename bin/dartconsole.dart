2827eda0c349d104e7f27763f21972e0577abc0f
adeelzahid1 <developer.adeelzahid@gmail.com>
2022-04-01 17:10:03 +0500

    yield dart genrator

--- bin/dartconsole.dart
+++ bin/dartconsole.dart
@@ -1,3 +1,14 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main() {
+  Stream<int> stream = boatStream(10);
+  stream.listen((data) =>{
+    print('Received Boat No $data'),
+  });
+}
+
+Stream<int> boatStream (int val) async*{
+  for(int i=1; i<=val; i++){
+    print("sent boat number $i");
+    await Future.delayed(Duration(seconds: 2),);
+    yield i;
+  }
 }
